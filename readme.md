ad3e1138b278acba16ab451624789df363475139
Maryna-Korbet <m22v86k@gmail.com>
2025-03-06 23:31:37 -0500

    added files

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Topic: Docker Technology Basics
+
+Create a virtual environment to install the library provisioning
+
+```bash
+pip install -r requirements.txt
+```
+
+Launching:
+
+```bash 
+python main.py
+```
+
---
+++ static/main.js
@@ -0,0 +1,26 @@
+function checkHealth() {
+  fetch('/healthchecker')
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((errorData) => {
+          throw new Error(errorData.detail || 'Unknown error')
+        })
+      }
+      return response.json()
+    })
+    .then((data) => {
+      console.log(data)
+      let responseDiv = document.getElementById('response')
+      responseDiv.innerHTML = data.message
+      responseDiv.className = 'response success'
+    })
+    .catch((error) => {
+      let errorMessage = error.message
+      if (errorMessage.startsWith('Error: ')) {
+        errorMessage = errorMessage.substring(7)
+      }
+      document.getElementById('response').innerHTML =
+        'Request error: ' + errorMessage
+      document.getElementById('response').className = 'response error'
+    })
+}
